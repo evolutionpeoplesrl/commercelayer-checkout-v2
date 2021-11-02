94c5f9a7e95dcfaf899455bf5513c51e3556dbbf
elenaghezzi <elena.ghezzi@evolutionpeople.it>
2021-11-02 16:19:03 +0100

    Google Ads Conversion Test 2

--- src/components/steps/DeliveryStep.vue
+++ src/components/steps/DeliveryStep.vue
@@ -78,8 +78,6 @@ export default {
     submit () {
       if (this.requires_payment) {
         this.nextStep()
-        gtag_report_conversion(window.location.href)
-        console.log('go')
       } else {
         this.$store.dispatch('placeOrder')
       }
